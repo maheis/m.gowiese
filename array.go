30f58200a8429473c9d077fe61869eb85c72d629
maheis <manfred@heister.email>
2023-10-24 21:58:48 +0200

    for-Schleife Beispiele

--- array.go
+++ array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func array() {
-	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 13}
 	b := a[2:5] // [3, 4, 5] (inklusive 2, exklusive 5)
 
 	fmt.Println("a:", a)
@@ -14,4 +14,21 @@ func array() {
 	// fmt.Println("ptrA[0]:", ptrA[0]) <- das geht nicht! Der Pointer muss dereferenziert werden.
 	c := *ptrA
 	fmt.Println("c:", c[0])
+
+	//Itterieren durch den Array, der Index i wird dabei durch den _ ignoriert!
+	for _, num := range a {
+		fmt.Print(num, ", ")
+	}
+	fmt.Println()
+
+	for i := range a {
+		fmt.Print(a[i], ", ")
+	}
+	fmt.Println()
+
+	//Itterieren durch den Array,
+	for i, num := range a {
+		fmt.Print(i, ": ", num, ", ")
+	}
+	fmt.Println()
 }

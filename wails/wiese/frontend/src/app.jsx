dd4fce72e686f063bc5132e8a284218f37d31007
maheis <manfred@heister.email>
2025-06-28 22:25:05 +0200

    feat: update logo image to new transparent version and clean up formatting

--- wails/wiese/frontend/src/app.jsx
+++ wails/wiese/frontend/src/app.jsx
@@ -1,7 +1,7 @@
 import './app.css';
-import logo from "./assets/images/logo-universal.png";
-import {Greet} from "../wailsjs/go/main/App";
-import {useState} from "preact/hooks";
+import logo from "./assets/images/frückteulen.papa.transparent.png";
+import { Greet } from "../wailsjs/go/main/App";
+import { useState } from "preact/hooks";
 
 export function App(props) {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
@@ -16,11 +16,11 @@ export function App(props) {
     return (
         <>
             <div id="App">
-                <img src={logo} id="logo" alt="logo"/>
+                <img src={logo} id="logo" alt="logo" />
                 <div id="result" className="result">{resultText}</div>
                 <div id="input" className="input-box">
                     <input id="name" className="input" onChange={updateName} autoComplete="off" name="input"
-                           type="text"/>
+                        type="text" />
                     <button className="btn" onClick={greet}>Greet</button>
                 </div>
             </div>

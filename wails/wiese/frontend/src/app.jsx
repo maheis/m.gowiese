c2d6e0e37ab49dd7097c74494e0980df18a882a5
maheis <manfred@heister.email>
2025-07-01 12:33:32 +0200

    documentation

--- wails/wiese/frontend/src/app.jsx
+++ wails/wiese/frontend/src/app.jsx
@@ -5,16 +5,31 @@ import { Greet } from "../wailsjs/go/main/App";
 import { useState } from "preact/hooks";
 
 export function App(props) {
-    const [resultColor, setResultColor] = useState('asdf');
+    // useState('') gibt ein Array zurück: [aktuellerWert, setFunktion].
+    // const [<VARIABLE>, set<VARIABLE-Functionsaufruf>] = useState('<STARTWERT>');
+    const [resultColor, setResultColor] = useState('');
     const [resultText, setResultText] = useState('Bitte gib deinen Namen ein 👇');
     const [name, setName] = useState('');
+
+    // function updateName(e) {
+    //     setName(e.target.value);
+    // }
+    // Arrow-Function:
     const updateName = (e) => setName(e.target.value);
+    // function updateResultColor(result) {
+    //     setResultColor(result);
+    // }
+    // Arrow-Function:
     const updateResultColor = (result) => setResultColor(result);
+    // function updateResultText(result) {
+    //     setResultText(result);
+    // }
+    // Arrow-Function:
     const updateResultText = (result) => setResultText(result);
 
     function greet(color) {
-        Greet(name).then(updateResultText);
         Color(color).then(updateResultColor);
+        Greet(name).then(updateResultText);
     }
 
     return (
